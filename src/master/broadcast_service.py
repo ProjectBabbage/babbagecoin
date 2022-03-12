abb1d75873d7420b62abeadc6202d780aa000dd7
yohannlefaou <yohannlefa@gmail.com>
2022-03-12 18:45:31 +0100

    remove your own url from broadcasted hots

--- src/master/broadcast_service.py
+++ src/master/broadcast_service.py
@@ -4,12 +4,10 @@ import requests
 from src.common.models import Block
 from src.common.schemas import BlockSchema
 
-known_hosts = [
-    "192.168.0.15",
-    # "192.168.0.19",
-    # "192.168.0.20",
-    # "192.168.0.21",
-]
+known_hosts = ["192.168.0.15",
+               "192.168.0.19",
+               "192.168.0.20",
+               "192.168.0.21",]
 
 url = os.environ.get("MYLOCALIP")
 url = f"http://{url}:5000"
@@ -17,10 +15,11 @@ url = f"http://{url}:5000"
 
 def broadcast_block(block: Block):
     for host in known_hosts:
-        bc = BlockSchema()
-        json_block_dict = {"url": url, "block": bc.dump(block)}
-        requests.post(
-            f"http://{host}:5000/blocks/updateblock",
-            json_block_dict,
-            headers={"Content-Type": "application/json"},
-        )
+        if host != url[7:-5]:
+            bc = BlockSchema()
+            json_block_dict = {"url": url, "block": bc.dump(block)}
+            requests.post(
+                f"http://{host}:5000/blocks/updateblock",
+                json_block_dict,
+                headers={"Content-Type": "application/json"},
+            )
