8cc73f0dcdb575c40ea86c11e88886c2e76beccc
Quentin Garchery <garchery.quentin@gmail.com>
2022-08-14 11:34:21 +0200

    remove balance checking for now

--- src/common/balances.py
+++ src/common/balances.py
@@ -18,14 +18,15 @@ def valid_balance(account: PubKey):
 
 
 def update_balances_from_transaction(stx: SignedTransaction, new: bool):
-    tx = stx.transaction
-    touch_balance(tx.sender)
-    if not new:
-        tx.amount = -tx.amount
-    balances[tx.sender.hash()] -= tx.amount
-    touch_balance(tx.receiver)
-    balances[tx.receiver.hash()] += tx.amount
-    return not (valid_balance(tx.sender) and valid_balance(tx.receiver))
+    # tx = stx.transaction
+    # touch_balance(tx.sender)
+    # if not new:
+    # tx.amount = -tx.amount
+    # balances[tx.sender.hash()] -= tx.amount
+    # touch_balance(tx.receiver)
+    # balances[tx.receiver.hash()] += tx.amount
+    # return not (valid_balance(tx.sender) and valid_balance(tx.receiver))
+    return False
 
 
 def update_balances_from_new_transaction(stx):
